d220c87621cb3b925108ca144bed7a9664254405
saman shaiza <samanshaiza@yahoo.com>
2023-11-08 17:47:30 -0600

    fixed titling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,29 +1,29 @@
-import React, { useContext, useState } from 'react'
-import GameOver from './GameOver';
-import '../styles/Modal.css';
-import { AppContext } from '../App';
+import React, { useContext, useState } from "react";
+import GameOver from "./GameOver";
+import "../styles/Modal.css";
+import { AppContext } from "../App";
 export default function Modal(props) {
-    const { gameOver, gameStart } = useContext(AppContext)
-    return (
-        <>
-          {gameOver.gameOver && 
-            <div className="modal">
-              <div className="overlay"></div>
-              <div className="modal-content">
-                <h1 className='modal-header'>Game Over</h1>
-                  {props.content}
-              </div>
-            </div>
-          } 
-          {!gameStart && 
-            <div className="modal">
-              <div className="overlay"></div>
-              <div className="modal-content">
-                <h1 className='modal-header'>pokemon.. ugh..</h1>
-                  {props.content}
-              </div>
-            </div>
-          } 
-        </>
-    );
+  const { gameOver, gameStart } = useContext(AppContext);
+  return (
+    <>
+      {gameOver.gameOver && (
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="modal-content">
+            <h1 className="modal-header">Game Over</h1>
+            {props.content}
+          </div>
+        </div>
+      )}
+      {!gameStart && (
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="modal-content">
+            <h1 className="modal-header">Pokemon Memory!</h1>
+            {props.content}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
